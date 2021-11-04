1303c00a7c90e6c6bfb1523ae59cc23a29e66ba6
Semih Dinc <sdinc@aum.edu>
2021-11-04 15:35:57 -0500

    Update test_robot_control.py

--- test_robot_control.py
+++ test_robot_control.py
@@ -9,16 +9,22 @@ import numpy as np
 
 #%% Here we define the Circular desired path function ------------------
 
-def desiredPathCircle(t):
-    x0 = 100 #100 cm radius circular motion
+def desiredPathCircle(t,totalSim):
+    x0 = 300 #mm radius circular motion
     y0 = 0
     
-    thetar = t*(2*np.pi/36)
-
+    thetar = t*(2*np.pi/totalSim)
     xr = np.cos(thetar)*x0 - np.sin(thetar)*y0
     yr = np.sin(thetar)*x0 + np.cos(thetar)*y0
     
-    qr = [xr,yr,thetar]
+    thetar_dot = 2*np.pi/totalSim
+    xr_dot = thetar_dot*-np.sin(thetar) - thetar_dot*np.cos(thetar)
+    yr_dot = thetar_dot*np.cos(thetar) - thetar_dot*np.sin(thetar)
+    
+    omega  = thetar_dot
+    vr     = np.sqrt(xr_dot*xr_dot+yr_dot*yr_dot)
+    
+    qr = [xr,yr,thetar,vr,omega]
     return qr
 
 #%% Here we define the Sin Wave desired path function ------------------
@@ -36,12 +42,13 @@ def desiredPathSineWave(t):
 
 #we create a simulation of 36 seconds.
 #simulation runs in every 0.1 seconds.
-simulation_time = np.arange(0,36,0.1)
+totalTime = 18 #secs
+simulation_time = np.arange(0,totalTime,1)
 
 #desired pose (trajectory) of the vehicle in every time instant
-qr = np.zeros([3,np.size(simulation_time)])
+qr = np.zeros([5,np.size(simulation_time)])
 for idx, t in enumerate(simulation_time):
-    qr[:,idx] = desiredPathCircle(t);
+    qr[:,idx] = desiredPathCircle(t,totalTime);
 
 #%% -----------------------------------------------------------
 from  pycreate2 import Create2
@@ -52,16 +59,20 @@ bot = Create2("COM3")
 bot.start() # Start the Create 2
 bot.safe()
 
-l = 23.5    #Wheel base of vehicle = 23.5 cm
-R = 50      #Distance to the rotational center in cm
+l = 235     #Wheel base of vehicle in mm
+R = 300     #Distance to the rotational center in mm
 
 #we calculate velocity instructions of left and right wheel
 # Vr/Vl cm must be travelled in every 0.1 seconds
-for idx, val in enumerate(simulation_time,1):
-    Vr = (R + l/2)*(qr[2,idx-1]-qr[2,idx-2]) 
-    Vl = (R - l/2)*(qr[2,idx-1]-qr[2,idx-2])
-    bot.drive_direct(10*Vr, 10*Vl)
-    time.sleep(0.1) #robot will go 10*V mm for 0.1 sec
+for idx, val in enumerate(simulation_time[1:],1):
+    #Vr = 
+    
+    Vr = (R + l/2)*(qr[2,idx]-qr[2,idx-1]) 
+    Vl = (R - l/2)*(qr[2,idx]-qr[2,idx-1])
+    print([idx, val, Vl, Vr])
+    
+    bot.drive_direct(int(Vr), int(Vl))
+    time.sleep(1) #robot will go V mm for 0.9 sec
 
 # Stop the bot
 bot.drive_stop()
