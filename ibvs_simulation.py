86972701d8f505e85c2d412eee40d758bb9e2097
Semih Dinc <sdinc@aum.edu>
2022-04-21 00:09:24 -0500

    Update ibvs_simulation.py

--- ibvs_simulation.py
+++ ibvs_simulation.py
@@ -38,12 +38,23 @@ def project_to_image_plane(pointCloud,camPose):
     pixel_coord = np.vstack([u,v])
     return pixel_coord
 
-#%% Constructs the jacobian matrix from image pixels and depth of each point
-def jacobian_matrix(pixel_coord,pixel_dist):
+#%% Point cloud projection to image plane
+def extract_2d_features(pointCloud,camPose):
+    world_to_cam = euler_trans(camPose)
     
-    u = pixel_coord[0,:]
-    v = pixel_coord[1,:]
-    z = pixel_dist
+    image_space_coord = np.linalg.multi_dot([world_to_cam, pointCloud])
+    x = image_space_coord[0,:]/image_space_coord[2,:]
+    y = image_space_coord[1,:]/image_space_coord[2,:]
+    
+    image_features = np.vstack([x,y])
+    return image_features
+
+#%% Constructs the jacobian matrix from image pixels and depth of each point
+def jacobian_matrix(pointCloud):
+    
+    u = pointCloud[0,:]
+    v = pointCloud[1,:]
+    z = pointCloud[2,:]
     
     J = np.zeros([len(z)*2, 6])
     
@@ -53,25 +64,63 @@ def jacobian_matrix(pixel_coord,pixel_dist):
 
     return J
 
+#%% This is the kinematic model of the diff drive for simulations
+def kinematicModel(u,q):
+    # Kinematic Model of the diff drive robot
+    l = 1; 
+    theta = q[2];
+    
+    xdot = l*np.cos(theta)*u[0]
+    ydot = l*np.sin(theta)*u[0]
+    thetadot = l*u[1]
+    
+    qdot = np.array([xdot, ydot, thetadot])
+    return qdot
+
 #%%
 #initial (q0) and desired (qr) pose of the vehicle
-q0 = np.array([2, 2, 15, 0, 0, 0])
+q0 = np.array([5, 10, 50, 0, 0, 0])
 qr = np.array([0, 0, 10, 0, 0, 0])
 
-pointCloud = np.array([[-1,-1,-1,1],
-                       [-1, 1,-1,1],
-                       [ 1, 1,-1,1],
-                       [ 1,-1,-1,1]]).T
+pointCloud = np.array([[-1,-1,1,1],
+                       [-1, 1,1,1],
+                       [ 1, 1,1,1],
+                       [ 1,-1,1,1]]).T
 
 actual_pixel_coord = project_to_image_plane(pointCloud, q0)
 des_pixel_coord = project_to_image_plane(pointCloud, qr)
 
-M = jacobian_matrix(actual_pixel_coord, pointCloud[2,:])
-
+plt.figure(0)
 plt.scatter(des_pixel_coord[0,:], des_pixel_coord[1,:],c ="blue")
 plt.plot(des_pixel_coord[0,[0,1,2,3,0]], des_pixel_coord[1,[0,1,2,3,0]])
 plt.scatter(actual_pixel_coord[0,:], actual_pixel_coord[1,:],c ="red")
 plt.plot(actual_pixel_coord[0,[0,1,2,3,0]], actual_pixel_coord[1,[0,1,2,3,0]])
 plt.xlim(0, 2*ox)
 plt.ylim(0, 2*oy) 
+
+#%%
+J = jacobian_matrix(pointCloud)
+inv_j = np.linalg.pinv(J)
+
+actual_features = extract_2d_features(pointCloud, q0)
+desired_features = extract_2d_features(pointCloud, qr)
+
+pixel_err = (actual_features - desired_features).T
+qdot = np.matmul(inv_j, pixel_err.flatten())
+    
+q1 = q0 + qdot
+
+#%%
+
+actual_pixel_coord = project_to_image_plane(pointCloud, q1)
+
+plt.figure(1)
+plt.scatter(des_pixel_coord[0,:], des_pixel_coord[1,:],c ="blue")
+plt.plot(des_pixel_coord[0,[0,1,2,3,0]], des_pixel_coord[1,[0,1,2,3,0]])
+plt.scatter(actual_pixel_coord[0,:], actual_pixel_coord[1,:],c ="red")
+plt.plot(actual_pixel_coord[0,[0,1,2,3,0]], actual_pixel_coord[1,[0,1,2,3,0]])
+plt.xlim(0, 2*ox)
+plt.ylim(0, 2*oy) 
+
+
 
